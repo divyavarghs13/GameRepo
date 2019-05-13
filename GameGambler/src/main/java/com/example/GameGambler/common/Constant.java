b8c7c8cc682098f146eedc74558460d9178ce30c
Divya <Divya@192.168.0.128>
2019-05-13 13:18:08 +0530

    <!--commit for output string -->

--- GameGambler/src/main/java/com/example/GameGambler/common/Constant.java
+++ GameGambler/src/main/java/com/example/GameGambler/common/Constant.java
@@ -1,10 +1,10 @@
 package com.example.GameGambler.common;
 
 public class Constant {
-
-	public static final String WIN_STATUS="WIN";
-	public static final String BONUS_STATUS="BONUS";
-	public static final String WIN_BONUS_STATUS="WIN_AND_BONUS";
-	public static final String LOSS="LOSS";
+	/** Setting the final result to display according to the status **/
+	public static final String WIN_STATUS="Congradulations, You have WON the game";
+	public static final String BONUS_STATUS="Congradulations, You have got BONUS Points, You can bet again without paying";
+	public static final String WIN_BONUS_STATUS="Congradulations, You have WON the game and got got BONUS Points, You can bet again without paying";
+	public static final String LOSS="Sorry, you LOST the game";
 	public static final String UNEXPECTED_ERROR="UNEXPECTED_ERROR";
 }
