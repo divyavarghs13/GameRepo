8d3dcbb3fb3de6892375309d9d534bbad0192163
Divya <Divya@192.168.0.128>
2019-05-13 17:43:35 +0530

    <!--Test case added -->

--- GameGambler/src/main/java/com/example/GameGambler/GameGamblerApplication.java
+++ GameGambler/src/main/java/com/example/GameGambler/GameGamblerApplication.java
@@ -3,9 +3,11 @@ package com.example.GameGambler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
+@Configuration
 public class GameGamblerApplication {
 
 	public static void main(String[] args) {
--- GameGambler/src/test/java/com/example/GameGambler/GameGamblerApplicationTests.java
+++ GameGambler/src/test/java/com/example/GameGambler/GameGamblerApplicationTests.java
@@ -1,16 +1,43 @@
 package com.example.GameGambler;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.GameGambler.service.PlayService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GameGamblerApplicationTests {
 
+	
+	@Autowired
+	private PlayService abcService;
+	
+	@Value("${winPercentage}")
+	private String winPercentage;
+	
+	@Value("${bonusPercentage}")
+	private String bonusPercentage;
+	
+	@Value("${winBonusPercentage}")
+	private String winBonusPercentage;
+	
 	@Test
-	public void contextLoads() {
+	public void fetchBetWinChanceTest(){
+		try {
+			String chance=abcService.fetchBetChance(winPercentage, bonusPercentage, winBonusPercentage);
+			System.out.println(chance);
+			assertEquals(chance, "WIN");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 	}
 
 }
